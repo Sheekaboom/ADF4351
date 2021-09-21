f505e9ab592aa51d23cc73df3ba7cda505da17be
Alec <Alec@weissworks.dev>
2021-09-21 15:25:57 -0600

    some minor changes to hopefully get a working example based on current directory structure

--- examples/interactive_test.ino
+++ examples/interactive_test.ino
@@ -1,26 +1,32 @@
 #include <SPI.h>
-#include "ADF4351.hpp"
+#include "./src/ADF4351.hpp"
 
 #define DATA_PIN 1 //not used on arduino
 #define CLK_PIN 1 //NOT USED on arduino
 #define SS_PIN 10
 
 
-void setup(){
+int main(){
+  //connect to our serial port
   Serial.begin(115200);
+  // connect to our frequency synthesizer
   ADF4351 myadf = ADF4351(DATA_PIN,CLK_PIN);
+  // initialize the synthesizer
   Serial.println(myadf.init());
   delay(500);
   Serial.println("test");
   myadf.print_info();
   delay(1000);
+  // set the power and write all of our registers
   myadf.set_output_power(RF_POW_m4);
   myadf.write_all();
+  //set the initial frequency
   //myadf.set_freq(1000e6);
   myadf.set_freq(955e6);
   myadf.print_info();
-  //I hate the arduino scoping
+  //I dislike the arduino scoping so I just put all of this in main
   while(1){
+    // change the frequency when entered by the user
     uint32_t freq_hz_in;//,freq_khz_in;
     delay(500);
     Serial.println("Please enter frequency to set in Hz");
@@ -31,7 +37,3 @@ void setup(){
   }
 }
 
-void loop(){
-
-//  myadf.set_freq(freq_hz_in);
-}
